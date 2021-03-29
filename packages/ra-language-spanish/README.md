2517aad1b6db4efebf0098b12d50c625ed2ea58f
JonatanSalas <jonatan.salas@blackbox-vision.tech>
2021-03-29 14:32:23 -0300

    feat(ra-language-spanish): migrate library to monorepo

--- packages/ra-language-spanish/README.md
+++ packages/ra-language-spanish/README.md
@@ -1,11 +1,76 @@
-# `ra-language-spanish`
+# RA Language Spanish [![License: MIT](https://img.shields.io/badge/License-MIT-brightgreen.svg)](https://opensource.org/licenses/MIT)
 
-> TODO: description
+Spanish translations for `React Admin`.
+
+## Install
+
+You can install this library via NPM or YARN.
+
+### NPM
+
+```bash
+npm i @blackbox-vision/ra-language-spanish
+```
+
+### YARN
+
+```bash
+yarn add @blackbox-vision/ra-language-spanish
+```
+
+## Use case
+
+You need to have spanish translations in your `React Admin` app.
 
 ## Usage
 
-```
-const raLanguageSpanish = require('ra-language-spanish');
+The usage is really simple:
 
-// TODO: DEMONSTRATE API
+1. Define your `i18nProvider`:
+
+```ts
+// i18nProvider.ts
+import polyglotI18nProvider from "ra-i18n-polyglot";
+import spanishMessages from "@blackbox-vision/ra-language-spanish";
+
+const messages = {
+  es: spanishMessages,
+};
+
+export const i18nProvider = polyglotI18nProvider((locale) => messages[locale]);
 ```
+
+2. Pass your custom `i18nProvider` to your `React Admin` instance:
+
+```tsx
+// App.tsx
+import React from "react";
+import { Admin, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+
+export const App = () => {
+  const dataProvider = jsonServerProvider(
+    "https://jsonplaceholder.typicode.com"
+  );
+
+  return (
+    <Admin locale="es" i18nProvider={i18nProvider} dataProvider={dataProvider}>
+      <Resource name="users" list={ListGuesser} />
+    </Admin>
+  );
+};
+
+App.displayName = "App";
+```
+
+## Issues
+
+Please, open an [issue](https://github.com/BlackBoxVision/react-admin-extensions/issues) following one of the issues templates. We will do our best to fix them.
+
+## Contributing
+
+If you want to contribute to this project see [contributing](https://github.com/BlackBoxVision/react-admin-extensions/blob/master/CONTRIBUTING.md) for more information.
+
+## License
+
+Distributed under the **MIT license**. See [LICENSE](https://github.com/BlackBoxVision/react-admin-extensions/blob/master/LICENSE) for more information.
--- packages/ra-language-spanish/src/index.ts
+++ packages/ra-language-spanish/src/index.ts
@@ -1,7 +1,153 @@
-'use strict';
+import { TranslationMessages } from "ra-core";
 
-module.exports = raLanguageSpanish;
+const spanishMessages: TranslationMessages = {
+  ra: {
+    action: {
+      add: "Añadir",
+      add_filter: "Añadir filtro",
+      back: "Ir atrás",
+      bulk_actions:
+        "1 item seleccionado |||| %{smart_count} items seleccionados",
+      cancel: "Cancelar",
+      clear_input_value: "Limpiar valor",
+      clone: "Clonar",
+      close: "Cerrar",
+      close_menu: "Cerrar menú",
+      confirm: "Confirmar",
+      create: "Crear",
+      delete: "Eliminar",
+      edit: "Editar",
+      expand: "Expandir",
+      export: "Exportar",
+      list: "Listar",
+      open_menu: "Abrir menú",
+      refresh: "Refrescar",
+      remove: "Borrar",
+      remove_filter: "Borrar filtro",
+      save: "Guardar",
+      search: "Buscar",
+      show: "Mostrar",
+      sort: "Ordenar",
+      undo: "Deshacer",
+      unselect: "Deseleccionar",
+    },
+    auth: {
+      auth_check_error: "Por favor inicie sesión para continuar",
+      logout: "Cerrar Sesión",
+      password: "Contraseña",
+      sign_in: "Acceder",
+      sign_in_error: "La autenticación falló, por favor, vuelva a intentarlo",
+      user_menu: "Perfil",
+      username: "Usuario",
+    },
+    boolean: {
+      true: "Sí",
+      false: "No",
+      null: " ",
+    },
+    input: {
+      file: {
+        upload_several:
+          "Arrastre algunos archivos para subir o haga clic para seleccionarlos.",
+        upload_single:
+          "Arrastre un archivo para subir o haga clic para seleccionarlo.",
+      },
+      image: {
+        upload_several:
+          "Arrastre algunas imagénes para subir o haga clic para seleccionarlas.",
+        upload_single:
+          "Arrastre alguna imagen para subir o haga clic para seleccionarla.",
+      },
+      references: {
+        all_missing: "No se pueden encontrar datos de referencias.",
+        many_missing:
+          "Al menos una de las referencias asociadas parece no estar disponible.",
+        single_missing: "La referencia asociada no parece estar disponible.",
+      },
+      password: {
+        toggle_visible: "Ocultar contraseña",
+        toggle_hidden: "Mostrar contraseña",
+      },
+    },
+    message: {
+      about: "Acerca de",
+      are_you_sure: "¿Está seguro?",
+      bulk_delete_content:
+        "¿Seguro que quiere eliminar este %{name}? |||| ¿Seguro que quiere eliminar estos %{smart_count} items?",
+      bulk_delete_title:
+        "Eliminar %{name} |||| Eliminar %{smart_count} %{name} items",
+      delete_content: "¿Seguro que quiere eliminar este item?",
+      delete_title: "Eliminar %{name} #%{id}",
+      details: "Detalles",
+      error:
+        "Se produjo un error en el cliente y su solicitud no se pudo completar",
+      invalid_form:
+        "El formulario no es válido. Por favor verifique si hay errores",
+      loading: "La página se está cargando, espere un momento por favor",
+      no: "No",
+      not_found:
+        "O bien escribió una URL incorrecta o siguió un enlace incorrecto.",
+      yes: "Sí",
+      unsaved_changes:
+        "Algunos de sus cambios no se guardaron. ¿Está seguro que quiere ignorarlos?",
+    },
+    navigation: {
+      next: "Siguiente",
+      no_more_results:
+        "El número de página %{page} está fuera de los límites. Pruebe la página anterior.",
+      no_results: "No se han encontrado resultados",
+      page_out_from_begin: "No puede ir antes de la página 1",
+      page_out_from_end: "No puede ir después de la última página",
+      page_out_of_boundaries: "Número de página %{page} fuera de los límites",
+      page_range_info: "%{offsetBegin} - %{offsetEnd} de %{total}",
+      page_rows_per_page: "Filas por página:",
+      prev: "Anterior",
+      skip_nav: "Saltar al contenido",
+    },
+    sort: {
+      sort_by: "Ordenar por %{field} %{order}",
+      ASC: "ascendente",
+      DESC: "descendente",
+    },
+    notification: {
+      bad_item: "Elemento incorrecto",
+      canceled: "Acción cancelada",
+      created: "Elemento creado",
+      data_provider_error:
+        "Error del proveedor de datos. Consulte la consola para más detalles.",
+      deleted: "Elemento borrado |||| %{smart_count} elementos borrados.",
+      http_error: "Error de comunicación con el servidor",
+      item_doesnt_exist: "El elemento no existe",
+      logged_out: "Su sesión ha finalizado, vuelva a conectarse.",
+      updated:
+        "Elemento actualizado |||| %{smart_count} elementos actualizados",
+      i18n_error:
+        "No se pudieron cargar las traducciones para el idioma especificado",
+    },
+    page: {
+      create: "Crear %{name}",
+      dashboard: "Tablero",
+      edit: "%{name} #%{id}",
+      empty: "Sin %{name} todavía.",
+      error: "Algo salió mal",
+      invite: "¿Quiere agregar una?",
+      list: "Lista de %{name}",
+      loading: "Cargando",
+      not_found: "No encontrado",
+      show: "%{name} #%{id}",
+    },
+    validation: {
+      email: "Debe ser un correo electrónico válido",
+      maxLength: "Debe contener %{max} caracteres o menos",
+      maxValue: "Debe ser %{max} o menos",
+      minLength: "Debe contener %{min} caracteres al menos",
+      minValue: "Debe ser al menos %{min}",
+      number: "Debe ser un número",
+      oneOf: "Debe ser uno de: %{options}",
+      regex: "Debe coincidir con un formato específico (regexp): %{pattern}",
+      required: "Requerido",
+    },
+  },
+};
 
-function raLanguageSpanish() {
-    // TODO
-}
+export default spanishMessages;
